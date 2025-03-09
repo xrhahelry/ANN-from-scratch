bc3ca6b57d2f37086f004075e9876124f66bda47
xrhahelry <sujalbajracharya1@gmail.com>
2025-03-09 21:10:01 +0545

    forward pass

--- main.jl
+++ main.jl
@@ -8,6 +8,7 @@ function min_max_normalize(arr)
 end
 
 relu(x) = max(0, x)
+sigmoid(x) = 1 / (1 + exp(-x))
 
 x1 = collect(1:100)
 x2 = x1 .^ 2
@@ -23,8 +24,10 @@ ax = Axis(fig[1, 1], title="Linear Regression Data", xlabel="X", ylabel="Y")
 scatter!(ax, x1, Y, label="Data", markersize=4)
 
 Random.seed!(69)
-W = randn(2)
-b = randn()
+W1 = reshape(randn(4), 2, 2)
+W2 = rand(2)
+b1 = randn(2)
+b2 = randn()
 epoch = 100
 lr = 1e-3
 l = length(x1)
@@ -32,23 +35,21 @@ buf = ones(l)
 
 dLw(Y, x, y) = transpose(x) * (-2(Y-y))
 dLb(Y, y) = transpose(-2(Y-y)) * buf
-accum = []
 
 for i in 1:epoch
     loss = 0
-    y = x*W .+ b
-    yy = relu.(y)
-    loss = transpose((Y-yy).^2)*buf
+    z = x*W1 .+ b1
+    Z = relu.(z)
+    y = Z*W2 .+ b2
+    y = relu.(y)
+    loss = transpose((Y-y).^2)*buf
     loss /= l
-    global W -= lr.*dLw(Y, x, yy)
-    global b -= lr*dLb(Y, y)
-    append!(accum, loss)
+    # global W -= lr.*dLw(Y, x, Z)
+    # global b -= lr*dLb(Y, y)
 end
-println("W1 = $(W[1]), W2 = $(W[2]), b = $(b)")
+# println("W1 = $(W[1]), W2 = $(W[2]), b = $(b)")
 
-outputs = W[1] .* x2 + W[2] .* x1 .+ b
-
-lines!(ax, x1, outputs, label="Fitted Line", linewidth=2, color=:red)
-Legend(fig[1,2], ax, "Legend")
-# lines!(fig[1,3], ax, collect(1:epoch), accum, label="Fitted Line", linewidth=2, color=:red)
-display(fig)
+# outputs = relu.(W[1] .* x2 + W[2] .* x1 .+ b)
+# scatter!(ax, x1, outputs, label="Fitted Line", markersize=4)
+# Legend(fig[1,2], ax, "Legend")
+# display(fig)
