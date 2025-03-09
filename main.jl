39340f80a9f268e972afab383e4e7b3fdb0f3afc
xrhahelry <sujalbajracharya1@gmail.com>
2025-03-09 21:44:31 +0545

    scalable neural network

--- main.jl
+++ main.jl
@@ -1,6 +1,52 @@
 using Random
 using CairoMakie
 
+mutable struct Layer
+    weights::Matrix{Float64}
+    biases::Vector{Float64}
+    activation::Function
+
+    function Layer(input_size::Int, output_size::Int, activation::Function)
+        weights = randn(output_size, input_size) * sqrt(2/input_size)
+        biases = zeros(output_size)
+        new(weights, biases, activation)
+    end
+end
+
+mutable struct NeuralNetwork
+    layers::Vector{Layer}
+
+    function NeuralNetwork()
+        new(Layer[])
+    end
+end
+
+function add_layer!(model::NeuralNetwork, input_size::Int, output_size::Int, activation::Function)
+    push!(model.layers, Layer(input_size, output_size, activation))
+end
+
+function forward(model::NeuralNetwork, inputs::Matrix{Float64})
+    outputs = inputs
+
+    for layer in model.layers
+        z = layer.weights * outputs .+ layer.biases
+        outputs = layer.activation.(z)
+    end
+
+    return outputs
+end
+
+function forward(model::NeuralNetwork, inputs::Vector{Float64})
+    outputs = inputs
+
+    for layer in model.layers
+        z = layer.weights * outputs .+ layer.biases
+        outputs = layer.activation.(z)
+    end
+
+    return outputs
+end
+
 function min_max_normalize(arr)
     min_val = minimum(arr)
     max_val = maximum(arr)
@@ -12,22 +58,18 @@ sigmoid(x) = 1 / (1 + exp(-x))
 
 x1 = collect(1:100)
 x2 = x1 .^ 2
-Y = (3 .* x1) + (7 .* x2) + (randn(100) .* 5)
+y = (3 .* x1) + (7 .* x2) + (randn(100) .* 5)
 
 x1 = min_max_normalize(x1)
 x2 = min_max_normalize(x2)
-x = hcat(x1, x2)
-Y = min_max_normalize(Y)
+X = Matrix(hcat(x1, x2)')
+y = min_max_normalize(y)
 
 fig = Figure()
 ax = Axis(fig[1, 1], title="Linear Regression Data", xlabel="X", ylabel="Y")
-scatter!(ax, x1, Y, label="Data", markersize=4)
+scatter!(ax, x1, y, label="Data", markersize=4)
 
 Random.seed!(69)
-W1 = reshape(randn(4), 2, 2)
-W2 = rand(2)
-b1 = randn(2)
-b2 = randn()
 epoch = 100
 lr = 1e-3
 l = length(x1)
@@ -36,20 +78,12 @@ buf = ones(l)
 dLw(Y, x, y) = transpose(x) * (-2(Y-y))
 dLb(Y, y) = transpose(-2(Y-y)) * buf
 
-for i in 1:epoch
-    loss = 0
-    z = x*W1 .+ b1
-    Z = relu.(z)
-    y = Z*W2 .+ b2
-    y = relu.(y)
-    loss = transpose((Y-y).^2)*buf
-    loss /= l
-    # global W -= lr.*dLw(Y, x, Z)
-    # global b -= lr*dLb(Y, y)
-end
-# println("W1 = $(W[1]), W2 = $(W[2]), b = $(b)")
+model = NeuralNetwork()
 
-# outputs = relu.(W[1] .* x2 + W[2] .* x1 .+ b)
-# scatter!(ax, x1, outputs, label="Fitted Line", markersize=4)
-# Legend(fig[1,2], ax, "Legend")
-# display(fig)
+add_layer!(model, 2, 2, relu)
+add_layer!(model, 2, 1, relu)
+
+output = forward(model, X)
+
+print(output)
+
